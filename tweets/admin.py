eb7059975821f5d6d12e451fbb7fc05b00bd73eb
vansnobal <97771752+vansnobal@users.noreply.github.com>
2024-10-01 20:30:31 +0900

    Assignment 26 Power Admin 오늘의 강의: 에어비앤비 클론코딩: From #8.0 to #8.4 제출기간: 익일 오전 6시까지 Mission: Tweet 그리고 Like models의 어드민을 개선하세요. Like Admin: (개선필요사항) Search by username of user foreign key. Filter by created_at Tweet Admin: (개선필요사항) Search by payload Search by username of user foreign key. Filter by created_at Make a CUSTOM filter for Tweets that contain and don't contain the words Elon Musk

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
 from tweets.models import Tweet, Like
 
+class ElonMuskFilter(admin.SimpleListFilter):
+    title = "Filter by keyword - Elon Musk"
+    parameter_name = 'keyword'
+    def lookups(self, request, model_admin):
+        return [("Elon Musk", "Elon Musk"),]
+    def queryset(self, request, tweets):
+        word = self.value()
+        if word:
+            return tweets.filter(payload__contains=word)
+        else:
+            return tweets
 
 # Register your models here.
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = ('payload', 'user', 'created_at', 'updated_at')
-    list_filter = ('user__username', 'created_at', 'updated_at')
+    list_filter = ('created_at', ElonMuskFilter)
+    search_fields = ('payload', 'user__username',)
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ('tweet', 'user', 'created_at', 'updated_at')
-    list_filter = ('user__username', 'created_at', 'updated_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username',)
